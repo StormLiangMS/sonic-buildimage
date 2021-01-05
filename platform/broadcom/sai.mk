d7a8f782d895de451870884aac841a6475a21d35
Judy Joseph <jujoseph@microsoft.com>
2021-01-05 18:43:13 +0000

    Merged PR 3946368: Update the Brcm SAI Debug image to 3.7.5.2-1
    Update the Brcm SAI Debug image to 3.7.5.2-1 in sai.mk file.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -4,9 +4,9 @@ BRCM_SAI_DEV = libsaibcm-dev_3.7.6.1-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.6.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=aUS4ZFCfD%2Bct29T%2B0xJAtFHfmtX1dTTsxKTyNtOw1O4%3D&se=2030-08-03T23%3A02%3A56Z&sp=r"
 
-BRCM_SAI_DBG = libsaibcm-dbg_3.7.5.2_amd64.deb
+BRCM_SAI_DBG = libsaibcm-dbg_3.7.5.2-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DBG)))
-$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dbg_3.7.5.2_amd64.deb?sv=2015-04-05&sr=b&sig=mReW7YrZTTTtGwDVb0x51oDAiOLv2VcJKOwDFkSari0%3D&se=2023-06-25T20%3A17%3A13Z&sp=r"
+$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dbg_3.7.5.2-1_amd64.deb?sv=2015-04-05&sr=b&sig=48sO%2B2C0Spw6Yk%2FDE1IqdY0z26p3bBnT%2Bx7Fd8i2EIo%3D&se=2034-09-14T18%3A38%3A57Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
