350154e80bfae026fb0f3b80f09404e93dd96ba8
Abhishek Dosi <abdosi@microsoft.com>
2021-08-07 01:00:30 +0000

    Updated Broadcom SAI debug debian package to 3.7.6.1
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -4,9 +4,9 @@ BRCM_SAI_DEV = libsaibcm-dev_3.7.6.1-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.6.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=aUS4ZFCfD%2Bct29T%2B0xJAtFHfmtX1dTTsxKTyNtOw1O4%3D&se=2030-08-03T23%3A02%3A56Z&sp=r"
 
-BRCM_SAI_DBG = libsaibcm-dbg_3.7.5.2-3_amd64.deb
+BRCM_SAI_DBG = libsaibcm-dbg_3.7.6.1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DBG)))
-$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dbg_3.7.5.2-3_amd64.deb?sv=2015-04-05&sr=b&sig=mRMfHopp%2FT077oVyNpWQoBm9hjXSH2QMZ1bXf34q5Zc%3D&se=2029-09-01T23%3A17%3A14Z&sp=r"
+$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dbg_3.7.6.1_amd64.deb?sv=2015-04-05&sr=b&sig=67eMh6kqEi%2BZdjkJb7AEsflPJwFZV2uWTAM%2B4WUGIG8%3D&se=2035-04-16T00%3A59%3A30Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
